1b11f08126b792e981da9459f126679bc2c2ad60
ikramiste <ikrame.saadi@gmail.com>
2023-10-03 10:24:13 +0200

    precisions added in comments

--- exo_02_V.rb
+++ exo_02_V.rb
@@ -1,5 +1,5 @@
 # exo_02_V.rb
 puts "Bonjour, comment tu t'appelles ?"
-print ">" #on insère ce symbole pour que l'utilisateur comprenne que c'est à lui de saisir du texte
+print ">" #on insère ce symbole pour que l'utilisateur comprenne que c'est à lui de saisir du texte (pas obligatoire mais user-friendly)
 user_name = gets.chomp #on demande un input à l'utilisateur avec gets.chomp et on donne un nom à ce qui sera saisi
 puts "Bonjour #{user_name} !" #on insère l'input de l'utilisateur dans la salutation, entre crochets pour que ce soit détecté comme tel (variable)
