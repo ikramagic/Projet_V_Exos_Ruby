f141d182d673706706f4c0560b6badb02d4154e7
ikramiste <ikrame.saadi@gmail.com>
2023-10-03 16:18:18 +0200

    edit for simple method

--- exo_13_V.rb
+++ exo_13_V.rb
@@ -1,9 +1,6 @@
 # exo_13_V.rb
-array_email = []
 
-50.times do |i|
-email = "jeanne.ausecours#{i}@email.com" 
-array_email << email #inclut chaque cycle de email et envoie ça pour être stockée dans l'array
-end
-
-puts array_email
+emails = [] #on crée l'array pour contenir les emails qui seront générés
+(1..50).each do |i| # i sera un chiffre partant de 1 pour arriver à 50
+    puts "jeanne.ausecours#{i}@email.com" # ça affichera l'adresse email en changeant i pour lui donner une valeur de 1 à 50
+  end
