7df6261f4a488a5f5a454a726bf7f371d79a024d
ikramiste <ikrame.saadi@gmail.com>
2023-10-03 18:01:31 +0200

    comments added

--- exo_16_V.rb
+++ exo_16_V.rb
@@ -5,8 +5,8 @@ print ">"
 
 rows = gets.chomp.to_i # on demande la valeur de rows à l'utilisateur
 
-for row in 0..rows
-	(rows-row).times {print " "}
-    row.times {print "#"}
-	puts
+for row in 0..rows # on demande à faire des rangées partant de 0 jusqu'à la valeur insérée
+	(rows-row).times {print " "} # ajoute des espaces vides au debut qui est egale à la valeur demandée - la rangée (ex: j'ai demandé 5, j'ajoute 5-, 5-2 espaces pour commencer)
+    row.times {print "#"} # demande les dièses dans les rangées, avec l'espaces
+	puts # fait apparaitre le tout 
 end
