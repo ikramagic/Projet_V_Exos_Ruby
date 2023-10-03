082d6e043e67e2cfcadd9e8e5959ea10372a72d6
ikramiste <ikrame.saadi@gmail.com>
2023-10-03 13:06:51 +0200

    fixed comments

--- exo_10_V.rb
+++ exo_10_V.rb
@@ -4,5 +4,6 @@ puts "Salut, t'es né(e) en quelle année toi ?"
 birth_year = gets.chomp.to_i
 
 (birth_year..2023).each do |one_year| #on crée une intervalle entre l'année donnée par l'utilisateur et on nomme chacune (each) des valeurs comprises dans l'intervalle = "one_year"
-    puts "En l'an #{one_year}, tu avais #{one_year - birth_year} ans !" #on demande d'afficher les années de la variable
-end                                                                     #ensuite une soustraction de l'année affichée avec l'année de naissance pour afficher l'âge durant l'année affichée
+    puts "En l'an #{one_year}, tu avais #{one_year - birth_year} ans !" 
+    #on demande d'afficher les années de la variable ensuite une soustraction pour calculer l'age à l'année indiquée dans one_year
+end
