fb6e17dfe9906f9b8b78ea60dd86b6aa80508611
ikramiste <ikrame.saadi@gmail.com>
2023-10-03 16:57:21 +0200

    pyramid present but limit and comments missing

--- exo_15_V.rb
+++ exo_15_V.rb
@@ -3,3 +3,10 @@
 
 puts "Salut, bienvenue dans ma super pyramide à base de larmes ! Combien d'étages veux-tu ?"
 print ">"
+
+rows = gets.chomp.to_i # on demande la valeur de rows à l'utilisateur
+
+for row in 0..rows 
+	row.times {print "*"}
+	puts
+end
