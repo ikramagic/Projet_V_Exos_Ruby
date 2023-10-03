5dc03a8894164560988644e039b8d113c7e4d0d6
ikramiste <ikrame.saadi@gmail.com>
2023-10-03 15:25:03 +0200

    first commit and comments

---
+++ exo_13_V.rb
@@ -0,0 +1,9 @@
+# exo_13_V.rb
+array_email = []
+
+50.times do |i|
+email = "jeanne.ausecours#{i}@email.com" 
+array_email << email #inclut chaque cycle de email et envoie ça pour être stockée dans l'array
+end
+
+puts array_email
