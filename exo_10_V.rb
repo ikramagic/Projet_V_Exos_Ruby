6a2cb79ac0417191403e7c971826ba5cac128bab
ikramiste <ikrame.saadi@gmail.com>
2023-10-03 13:12:23 +0200

    comments fixed again

--- exo_10_V.rb
+++ exo_10_V.rb
@@ -5,5 +5,5 @@ birth_year = gets.chomp.to_i
 
 (birth_year..2023).each do |one_year| #on crée une intervalle entre l'année donnée par l'utilisateur et on nomme chacune (each) des valeurs comprises dans l'intervalle = "one_year"
     puts "En l'an #{one_year}, tu avais #{one_year - birth_year} ans !" 
-    #on demande d'afficher les années de la variable ensuite une soustraction pour calculer l'age à l'année indiquée dans one_year
+    #on demande d'afficher les années de la variable ensuite une soustraction pour calculer l'age à l'année indiquée, tout s'affiche car it falls under "puts" grace aux accolades
 end
